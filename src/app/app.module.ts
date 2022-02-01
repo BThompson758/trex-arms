44a59ec843eae79162827e2342ae4173363316dc
Robert <bobby.bsa758@gmail.com>
2022-02-01 18:14:43 -0500

    Lab Week 3 turn in - extra navbar & card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { AppComponent } from './app.component';
 import { MidnavComponent } from './header/midnav.component';
 import { Nav1Component } from './header/nav1.component';
 import { Nav2Component } from './header/nav2.component';
+import { DeltaPointPro } from './products/dpp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Nav1Component,
     Nav2Component,
-    MidnavComponent
+    MidnavComponent,
+    DeltaPointPro
   ],
   imports: [
     BrowserModule
---
+++ src/app/products/dpp.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'tx-dpp',
+    templateUrl: 'dpp.component.html',
+    styleUrls: ['dpp.component.css']
+})
+
+export class DeltaPointPro{}
