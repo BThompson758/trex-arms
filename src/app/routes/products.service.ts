79833b91aeda1237d761a9e897e5779305e8b1a9
Robert Thompson <bobby.bsa758@gmail.com>
2022-03-22 14:37:26 -0400

    Build fixes

--- src/app/routes/products.service.ts
+++ src/app/routes/products.service.ts
@@ -1,12 +1,12 @@
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { Component, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { ProductModel } from "../products/product.model";
 
 @Injectable(
     { providedIn: 'root' }
 )
 export class ProductService {
-    constructor(private db:AngularFireDatabase)) {
+    constructor(private db:AngularFireDatabase) {
     }
 
     getProductList(page: string) {
